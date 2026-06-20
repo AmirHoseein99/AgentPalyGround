85749ada5a94cd962f1e53c26f7a387faa151c5b
AmirHoseein99 <amirimani491@gmail.com>
2026-06-20 09:54:04 +0330

    feat: :sparkles: Add logging for OpenRouter API calls to enhance traceability

--- src/llm/openrouter.py
+++ src/llm/openrouter.py
@@ -1,12 +1,13 @@
 from encodings.punycode import T
 import requests
 from ..core.config import setting
-
+from ..logger import get_logger
 
 class OpenRouterAPI:
     def __init__(self) -> None:
         self.url = f"{setting.OPENROUTER_API_BASE_URL}/chat/completions"
-            
+        self.logger = get_logger("openrouter")
+
     def stream_openrouter_response(self, messages):
         session = requests.sessions.Session()
         headers = {
@@ -33,6 +34,7 @@ class OpenRouterAPI:
             "model": setting.OPENROUTER_MODEL,
             "messages": messages
         }
+        self.logger.info(f"calling openrouter api with header : {headers}, data : {data}")
         response = requests.post(self.url, headers=headers, json=data)
         if response.status_code == 200:
             return response.json()
