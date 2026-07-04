2f0f868b809fbc154d37d83501cfebb289d68c27
AmirHoseein99 <amirimani491@gmail.com>
2026-07-04 09:53:06 +0330

    refactor: :recycle: Add custom exception classes for improved error handling

---
+++ src/exceptions.py
@@ -0,0 +1,21 @@
+class ToolValidationMissError(Exception):
+    def __init__(self, message) -> None:
+        super().__init__(message)
+
+class ParserError(Exception):
+    def __init__(self, message) -> None:
+        super().__init__(message)
+
+class ToolValidationError(Exception):
+    def __init__(self, message) -> None:
+        super().__init__(message)
+
+
+class ToolExecutionError(Exception):
+    def __init__(self, message) -> None:
+        super().__init__(message)
+
+
+class LLMError(Exception):
+    def __init__(self, message) -> None:
+        super().__init__(message)
